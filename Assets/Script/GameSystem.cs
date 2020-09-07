68e9c386df41d99d0b95f84374b87541384f34cc
大暉 永原 <h1201919007@hiratagakuen.onmicrosoft.com>
2020-09-07 15:56:53 +0900

    タイトルからゲームへの移行できた

---
+++ Assets/Script/GameSystem.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameSystem : MonoBehaviour
+{
+    //　スタートボタンを押したら実行する
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+
+
+    }
+}
