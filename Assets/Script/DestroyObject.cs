b1b46419a0cf6baee316076b2421086feb230ae1
大暉 永原 <h1201919007@hiratagakuen.onmicrosoft.com>
2020-09-09 15:25:32 +0900

    DestoryEnemy途中

--- Assets/Script/DestroyObject.cs
+++ Assets/Script/DestroyObject.cs
@@ -7,11 +7,12 @@ public class DestroyObject : MonoBehaviour
 
     public int damage;          //当たった部位毎のダメージ量
     private GameObject enemy;   //敵オブジェクト
-    
+    private DestroyEnemy hp;              //HPクラス
 
     void Start()
     {
         enemy = GameObject.Find("Enemy");   //敵情報を取得
+        hp = enemy.GetComponent<DestroyEnemy>();      //HP情報を取得
     }
 
     void OnTriggerEnter(Collider other)
@@ -20,6 +21,9 @@ public class DestroyObject : MonoBehaviour
         //ぶつかったオブジェクトのTagにShellという名前が書いてあったならば（条件）.
         if (other.CompareTag("Shell"))
         {
+
+            //HPクラスのDamage関数を呼び出す
+            hp.Damage(damage);
 
             //ぶつかってきたオブジェクトを破壊する.
             Destroy(other.gameObject);
