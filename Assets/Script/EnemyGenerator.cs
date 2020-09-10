aab8bf8ec085ea592033041ae02bd34d09d6cb0a
M.Ryuya <56579452+MiyoshiRyuya@users.noreply.github.com>
2020-09-10 11:06:06 +0900

    スポーン関係

--- Assets/Script/EnemyGenerator.cs
+++ Assets/Script/EnemyGenerator.cs
@@ -6,6 +6,26 @@ public class EnemyGenerator : MonoBehaviour
 {
     //敵プレハブ
     public GameObject enemyPrefab;
+    //敵プレハブ
+    public GameObject enemyPrefab2;
+    //敵プレハブ
+    public GameObject enemyPrefab3;
+    //時間間隔の最小値
+    public float minTime = 2f;
+    //時間間隔の最大値
+    public float maxTime = 5f;
+    //X座標の最小値
+    public float xMinPosition = -10f;
+    //X座標の最大値
+    public float xMaxPosition = 10f;
+    //Y座標の最小値
+    public float yMinPosition = 0f;
+    //Y座標の最大値
+    public float yMaxPosition = 10f;
+    //Z座標の最小値
+    public float zMinPosition = 10f;
+    //Z座標の最大値
+    public float zMaxPosition = 20f;
     //敵生成時間間隔
     private float interval;
     //経過時間
@@ -15,7 +35,7 @@ public class EnemyGenerator : MonoBehaviour
     void Start()
     {
         //時間間隔を決定する
-        interval = 10.0f;
+        interval = GetRandomTime();
     }
 
     // Update is called once per frame
@@ -29,10 +49,30 @@ public class EnemyGenerator : MonoBehaviour
         {
             //enemyをインスタンス化する(生成する)
             GameObject enemy = Instantiate(enemyPrefab);
-            //生成した敵の座標を決定する(現状X=0,Y=10,Z=20の位置に出力)
-            enemy.transform.position = new Vector3(0, 10, 20);
+            //生成した敵の位置をランダムに設定する
+            enemy.transform.position = GetRandomPosition();
             //経過時間を初期化して再度時間計測を始める
             time = 0f;
+            //次に発生する時間間隔を決定する
+            interval = GetRandomTime();
         }
+    }
+
+    //ランダムな時間を生成する関数
+    private float GetRandomTime()
+    {
+        return Random.Range(minTime, maxTime);
+    }
+
+    //ランダムな位置を生成する関数
+    private Vector3 GetRandomPosition()
+    {
+        //それぞれの座標をランダムに生成する
+        float x = Random.Range(xMinPosition, xMaxPosition);
+        float y = Random.Range(yMinPosition, yMaxPosition);
+        float z = Random.Range(zMinPosition, zMaxPosition);
+
+        //Vector3型のPositionを返す
+        return new Vector3(x, y, z);
     }
 }
